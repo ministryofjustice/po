acdd42cb80e3ff421fc392c7f893a9bf405d121f
Andy Driver <andy@pagezero.net>
2015-10-13 11:48:09 +0100

    Add script to generate DPKG dependencies JSON payload

---
+++ scripts/dpkg-dependencies.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+value() {
+    echo "$1" | cut -f2 -d' '
+}
+
+startswith() {
+    case $2 in $1*) true;; *) false;; esac;
+}
+
+filter_name_and_version() {
+    IFS=$'\n'
+    local metadata=( $1 )
+    for line in "${metadata[@]}"; do
+
+        if startswith Package "$line"; then
+            local name=$(value "$line")
+
+        elif startswith Version "$line"; then
+            local version=$(value "$line")
+            echo "$name|$version"
+        fi
+    done
+    unset IFS
+}
+
+json_pkg() {
+    local name="${1%|*}"
+    local ver="${1#*|}"
+    printf '{"name": "%s", "version": "%s", "source": "apt"}' "$name" "$ver"
+}
+
+json_format() {
+    printf "{\"packages\": ["
+    for pkg in "$@"; do
+        if [ "$pkg" != "$1" ]; then printf ", "; fi
+        json_pkg "$pkg"
+    done
+    printf "]}\n"
+}
+
+installed_packages_metadata() {
+    dpkg --status $(dpkg --get-selections | cut -f1)
+}
+
+json_format $(filter_name_and_version "$(installed_packages_metadata)")
